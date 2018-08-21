edb7fc670b8cd8f297180808d4e5fdddb808df5a
Exatiel-Sandoval <Exatiel@gmail.com>
2018-08-21 14:39:53 -0700

    location and some security

--- livefeed.js
+++ livefeed.js
@@ -5,46 +5,60 @@
 -feed is endless log of peoples thoughts(posts)
 -after someone posts the input is cleared and someone else can post
 */
-var post, input, characters, maxChar;
-maxChar = 150;
-//check if there is any input in the box
-document.querySelector('.btn-submit').addEventListener('click', function() {
-  //read the input field
-  input = document.querySelector('.input-field').value;
-  characters = input.length;
-  console.log(characters);
-  //check if there is something in it and then set it = to post
-  if (input) {
-    //check how many characters are in the input
-    if(characters <= maxChar){
-    post = input;
-    //update the ui to show the post
-    //document.querySelector('.posts').textContent = post;
 
-    document.querySelector('.input-field').value = '';
-    newPost();
-  }else{}
+(function() {
+  var post, input, characters, maxChar;
+  
+  function onPositionReceived(position){
+    console.log(position);
+  }
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onPositionReceived);
+  }
+  //maxChar = 150;
+  //check if there is any input in the box
+  document.querySelector('.btn-submit').addEventListener('click', function() {
+    //read the input field
+    input = document.querySelector('.input-field').value;
+    //characters = input.length;
+    //console.log(characters);
+    //check if there is something in it and then set it = to post
+    if (input) {
+      //check how many characters are in the input
+    //  if(characters <= maxChar){
+      post = input;
+      //update the ui to show the post
+      //document.querySelector('.posts').textContent = post;
 
-    /*var div = document.createElement("div");
-    div.style.width = "100vw";
-    div.style.height = "50px";
-    div.style.background = "whitesmoke";
-    div.style.color = "black";
+      document.querySelector('.input-field').value = '';
+      newPost();
+    }else{
+
+    }
+
+      /*var div = document.createElement("div");
+      div.style.width = "100vw";
+      div.style.height = "50px";
+      div.style.background = "whitesmoke";
+      div.style.color = "black";
+      div.textContent = post;
+
+      document.getElementById('feed').prepend(div);
+  */
+    };
+  });
+
+  function newPost() {
+    var div = document.createElement("div");
+    div.class = 'posts'
+    //div.style.width = "100vw";
+    //div.style.height = "50px";
+    //div.style.background = "whitesmoke";
+    //div.style.color = "black";
     div.textContent = post;
 
     document.getElementById('feed').prepend(div);
-*/
-  };
-});
+  }
 
-function newPost() {
-  var div = document.createElement("div");
-  div.class = 'posts'
-  //div.style.width = "100vw";
-  //div.style.height = "50px";
-  //div.style.background = "whitesmoke";
-  //div.style.color = "black";
-  div.textContent = post;
 
-  document.getElementById('feed').prepend(div);
-}
+}());
